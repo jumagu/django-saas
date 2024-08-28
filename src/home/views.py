cbf6bd6270aa6a2c5ed046062441cfe539c94b40
jumagu <jmgg.gutierrez@gmail.com>
2024-08-28 18:40:11 -0500

    chore: Update home page URLs and views

--- src/home/views.py
+++ src/home/views.py
@@ -6,16 +6,24 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all() # queryset
     page_qs = PageVisit.objects.filter(path=request.path)
+    
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     
     my_title = "My Page"
     my_context = {
         "page_title": my_title,
         "page_visits_count": page_qs.count(),
         "total_visits_count": qs.count(),
-        "percent": (page_qs.count() * 100.0) / qs.count()
+        "percent": percent
     }
     html_template = "home.html"
     
