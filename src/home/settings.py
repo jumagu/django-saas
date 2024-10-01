4a116c17c254a3a94e071300689069c0247e3409
jumagu <jmgg.gutierrez@gmail.com>
2024-10-01 13:40:53 -0500

    refactor: Update requirements.txt and settings.py for static file management with whitenoise

--- src/home/settings.py
+++ src/home/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -151,6 +152,14 @@ STATICFILES_DIRS = [
 
 # Output for python manage.py collectstatic
 STATIC_ROOT = BASE_DIR / "local-cdn"
+# < django 4.2
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
