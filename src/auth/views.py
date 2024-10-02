9509e5d93508a7bccce4b722eca46a7668409134
jumagu <jmgg.gutierrez@gmail.com>
2024-10-01 23:32:24 -0500

    feat: add registration view

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
+# ! Not recommended - Can't customize
+# from django.contrib.auth.models import User
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 def login_view(request):
     # This is just for learn purpose
     # There are built-in mechanism validate request data in Django
@@ -17,5 +24,21 @@ def login_view(request):
         
     return render(request, 'auth/login.html', {})
 
-# def register_view(request):
-#     return render(request, 'auth/register.html', {})
+def register_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email") or None
+        username = request.POST.get("username") or None
+        password = request.POST.get("password") or None
+        
+        if all([email, username, password]):
+            # Django forms performs this easily
+            # email_exist = User.objects.filter(email__iexact=email).exists()
+            # username_exist = User.objects.filter(username__iexact=username).exists()
+            
+            try:
+                User.objects.create_user(username=username, email=email, password=password)
+            except:
+                pass
+            
+        
+    return render(request, 'auth/register.html', {})
