ce1637ee56a7e585d5a13dbaf89bb76ebe64af8c
jumagu <jmgg.gutierrez@gmail.com>
2024-12-14 23:38:01 -0500

    feat: add helpers related to checkout process with stripe

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -48,3 +48,43 @@ def create_price(
     if raw:
         return response
     return response.id # stripe product id
+
+def start_checkout_session(
+    customer_id='',
+    success_url='',
+    stripe_price_id='',
+    cancel_url='',
+    raw=False
+):
+    if not success_url.endswith('?session_id={CHECKOUT_SESSION_ID}'):
+        success_url = f'{success_url}' + '?session_id={CHECKOUT_SESSION_ID}'
+    response = stripe.checkout.Session.create(
+        customer=customer_id,
+        success_url=success_url,
+        cancel_url=cancel_url,
+        line_items=[{"price": stripe_price_id, "quantity": 1}],
+        mode="subscription",
+    )
+    if raw:
+        return response
+    return response.url
+
+def get_checkout_session(stripe_id, raw=False):
+    response = stripe.checkout.Session.retrieve(stripe_id)
+    if raw:
+        return response
+    return response.url
+
+def get_subscription(stripe_id, raw=False):
+    response = stripe.Subscription.retrieve(stripe_id)
+    if raw:
+        return response
+    return response.url
+
+def get_checkout_customer_plan(session_id):
+    checkout_res = get_checkout_session(stripe_id=session_id, raw=True)
+    customer_id = checkout_res.customer
+    sub_stripe_id = checkout_res.subscription
+    sub_res = get_subscription(sub_stripe_id, raw=True)
+    sub_plan = sub_res.plan
+    return customer_id, sub_plan.id
