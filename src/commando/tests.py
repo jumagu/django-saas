063031bc82bff66fac6f9060cbb35e5549afbc94
jumagu <jmgg.gutierrez@gmail.com>
2024-12-19 18:58:01 -0500

    test: add a simple test to verify if the env var DATABASE_URL exists and is valid

--- src/commando/tests.py
+++ src/commando/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.conf import settings
 
-# Create your tests here.
+class SupabaseDBTestCase(TestCase):
+    def test_db_url(self):
+        DATABASE_URL = settings.DATABASE_URL
+        self.assertIn('pooler.supabase', DATABASE_URL)
