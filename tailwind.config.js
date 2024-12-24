fdebb43f898139e74f949e0568a617515f788eaf
jumagu <jmgg.gutierrez@gmail.com>
2024-12-24 13:20:08 -0500

    feat: add safelist for alert classes in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/templates/**/*.{html,js}'],
+  safelist: [
+    'alert-info',
+    'alert-success',
+    'alert-warning',
+    'alert-error',
+  ],
   theme: {
     extend: {},
   },
